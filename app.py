9e64c4e25553fcd86a5b06e37daf3a582d73f45a
Vadim Romaniuk <vadim.romanyuk.2003@gmail.com>
2024-05-25 11:57:18 +0300

    add session functions

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 # pip install flask
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "5f352379324c22463451387a0aec5d2f"
 
 @app.route('/')
-def indexView():
-    return render_template('pages/index.html')
+def toZeroView():
+    if 'lich' not in session:
+        session['lich'] = 0
+    else:
+        session['lich'] = 0
+        
+    return render_template('pages/toZero.html', lich=session['lich'])
 
-@app.route('/contact')
-def contactView():
-    return render_template('pages/contact.html')
+@app.route('/plus')
+def plusPlusView():
+    if 'lich' not in session:
+        session['lich'] = 0
+    
+    session['lich'] += 1
+    message = 'Ну привіт'
+    return render_template('pages/plusPlus.html', lich=session['lich'], message=message)
 
-@app.route('/about')
-def aboutView():
-    return render_template('pages/about.html')
+@app.route('/minus')
+def minusMinusView():
+    if 'lich' not in session:
+        session['lich'] = 0
+    
+    session['lich'] -= 1
+    return render_template('pages/minusMinus.html', lich=session['lich'])
 
 
 if __name__ == "__main__":
